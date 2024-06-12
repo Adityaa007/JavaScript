ed965a0d8e72b53a387794826f8e2a6c8e19534d
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-12 07:46:06 +0000

    updated

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let accountEmail="mymail@gmail.com"
+var accountPassword="12345"
+
+accountcity="Jaipur"
+let accountState
+// accountId=3   //not allowed
+
+accountEmail="h23r@gmail.com"
+accountPassword="32232"
+accountcity="Banglore"
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
+
+/*
+prefer not to use Var
+because of the issue in block scope and functional scope
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all js code as new version
+// alert("Hello") //we are using nodejs not browser
+console.log(3+3)
+
+console.log("hitesh");
+
+/*
+number >= 2 to power 53
+big int
+string >= ""
+boolean >= true/false
+null >= standalone value
+undefined
+symbol
+*/
