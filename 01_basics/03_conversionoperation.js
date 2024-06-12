10aef014731fd930a347f1e77bbaff35814825e7
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-12 10:56:54 +0000

    updated

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,24 +1,24 @@
-// let score=33
-// console.log(typeof score);
+let score=33
+console.log(typeof score);
 
-// let valueinNumber= Number(score)
-// console.log(typeof valueinNumber);
+let valueinNumber= Number(score)
+console.log(typeof valueinNumber);
 
-// //"33">= 33
-// // "33abc">=NaN
-// //true >= 1 ; false >= 0
+//"33">= 33
+// "33abc">=NaN
+//true >= 1 ; false >= 0
 
-// let isLoggedIn=1
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn)
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
 
-// // 1 >= true ; 0 >= false
-// // " " >= false ; "histe">= true
+// 1 >= true ; 0 >= false
+// " " >= false ; "histe">= true
 
-// let someNumber =33
-// let stringNumber=String(someNumber)
-// console.log(stringNumber)
-// console.log(typeof stringNumber)
+let someNumber =33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
 
 // ***************************Operations*****************************//
 
