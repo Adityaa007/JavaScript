4ae08f24a4f0d16d11caeea2bd721269ad9af8fa
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-12 10:55:32 +0000

    updated

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,21 +1,43 @@
-let score=33
-console.log(typeof score);
+// let score=33
+// console.log(typeof score);
 
-let valueinNumber= Number(score)
-console.log(typeof valueinNumber);
+// let valueinNumber= Number(score)
+// console.log(typeof valueinNumber);
 
-//"33">= 33
-// "33abc">=NaN
-//true >= 1 ; false >= 0
+// //"33">= 33
+// // "33abc">=NaN
+// //true >= 1 ; false >= 0
 
-let isLoggedIn=1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn=1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 >= true ; 0 >= false
-// " " >= false ; "histe">= true
+// // 1 >= true ; 0 >= false
+// // " " >= false ; "histe">= true
 
-let someNumber =33
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber =33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***************************Operations*****************************//
+
+let value = 3
+let negvalue=-value
+console.log(negvalue);
+
+let str1="hello"
+let str2=" Aditya"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
+++gamecounter
+console.log(gamecounter);
