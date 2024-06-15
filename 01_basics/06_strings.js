63c586e8afdacc62b58a72b3f52345f846fa08bd
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-15 10:31:28 +0000

    updated

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,20 @@
+const name = "Aditya"
+const repoCount=50
+
+//  console.log(name+repoCount+"value");
+
+console.log(`Hello my name is ${name} and my repCount is ${repoCount}`);
+console.log(name.toUpperCase());
+console.log(name);
+console.log(name.charAt(2));
+console.log(name.indexOf('t'));
+
+const str="      hello      "
+console.log(str);
+console.log(str.trim());
+
+const url="https://hitesh.com/hitesh%20Chaudhary"
+console.log(url.replace('%20','-'))
+
+const nm="hitesh-ur-comdf"
+console.log(nm.split('-'));
