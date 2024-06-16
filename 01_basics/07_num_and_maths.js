e049a04ae493a5146ea4237715298e02e37faced
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-06-16 09:53:31 +0000

    updated

---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,33 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance );
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const num=23.8699
+
+console.log(num.toPrecision(3));
+
+const hundred=100000
+console.log(hundred.toLocaleString('en-IN'));
+
+//*******************************MAths*******************************//
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.4));
+console.log(Math.min(4,3,5,6,2,0));
+console.log(Math.max(4,34,43,56,78));
+console.log(Math.random());
+console.log(Math.random());
+
+
+const min=10
+const max=20
+
+console.log(Math.random()*(max-min+1)+min)
+
