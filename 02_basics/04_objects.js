7d381d217f845dcf28525829355ebf84dc3c3f29
Aditya Suyal <117057991+Adityaa007@users.noreply.github.com>
2024-09-03 11:31:31 +0000

    updated

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -19,3 +19,10 @@ console.log(regularUser.fullname);
 console.log(regularUser.fullname.userfullname);
 console.log("email:",regularUser.email);
 
+const obj1={1:"a",2:"b",3:"c"}
+const obj2={4:"d",5:"e",6:"f"}
+const obj3=Object.assign({},obj1,obj2)
+console.log(obj3);
+const obj4={...obj1,...obj2}
+console.log(obj4);
+
